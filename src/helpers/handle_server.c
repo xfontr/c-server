25e913325885c56233cba5c5117fcd19e6bcce1c
xfontr <xifre@Mac.technicolor.net>
2025-02-05 19:31:52 +0100

    Update errors

--- src/helpers/handle_server.c
+++ src/helpers/handle_server.c
@@ -15,7 +15,7 @@ int accept_connection(int socketfd);
 int enable_port_reuse(int socketfd);
 int close_socket(int socketfd);
 struct sockaddr_in get_bind_address();
-int check_errors(int result, int socketfd, short error_code);
+int check_errors(int result, short error_code);
 
 struct sockaddr_in get_bind_address()
 {
@@ -25,54 +25,43 @@ struct sockaddr_in get_bind_address()
         .sin_port = htons(PORT)};
 }
 
-int close_socket(int socketfd)
-{
-    if (socketfd != INT_MIN & close(socketfd) < 0)
-    {
-        handle_error(ERROR_CLOSING);
-        return -1;
-    };
-
-    return 0;
-}
-
-int check_errors(int result, int socketfd, short error_code)
+int check_errors(int result, short error_code)
 {
     if (result < 0)
     {
         handle_error(error_code);
-        close_socket(socketfd);
         return -1;
     }
 
     return result;
 }
 
+int close_socket(int socketfd)
+{
+    return check_errors(close(socketfd), ERROR_SOCKET_CLOSING);
+}
+
 int enable_port_reuse(int socketfd)
 {
     int enable = 1;
-    int set_options_result = setsockopt(socketfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(enable));
-    return check_errors(set_options_result, socketfd, ERROR_REUSABLE_SOCKET);
+    return setsockopt(socketfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(enable));
 }
 
 int create_socket()
 {
-    int socketfd = socket(AF_INET, SOCK_STREAM, 0);
-    return check_errors(socketfd, socketfd, ERROR_SOCKET_CREATION);
+    return socket(AF_INET, SOCK_STREAM, 0);
 }
 
 int bind_socket(int socketfd)
 {
     struct sockaddr_in server_address = get_bind_address();
 
-    int binding = bind(socketfd, (struct sockaddr *)&server_address, sizeof(server_address));
-
-    return check_errors(binding, socketfd, ERROR_BINDING);
+    return bind(socketfd, (struct sockaddr *)&server_address, sizeof(server_address));
 }
 
 int init_server(int socketfd)
 {
-    return check_errors(listen(socketfd, MAX_CONNECTIONS), socketfd, ERROR_LISTENING);
+    return listen(socketfd, MAX_CONNECTIONS);
 }
 
 int accept_connection(int socketfd)
@@ -82,7 +71,7 @@ int accept_connection(int socketfd)
 
     int new_socket = accept(socketfd, (struct sockaddr *)&client_address, &client_size);
 
-    int result = check_errors(new_socket, INT_MIN, ERROR_ACCEPT_CONNECTION);
+    int result = check_errors(new_socket, ERROR_SOCKET_CLIENT_CONNECTION);
 
     if (result >= 0)
     {
